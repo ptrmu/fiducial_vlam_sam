726fe80e90005018cbd8d049280d372ce982a12b
Peter Mullen <ptrmu@outlook.com>
2019-12-04 16:03:12 -0800

    Tweak to run on eloquent

--- fiducial_vlam/launch/test_sam_map_launch.py
+++ fiducial_vlam/launch/test_sam_map_launch.py
@@ -23,10 +23,10 @@ corner_measurement_sigma = 1.0
 
 tello_args = [{
     'drone_ip': '192.168.0.30',
-    'command_port': '11002',
-    'drone_port': '8889',
-    'data_port': '13002',
-    'video_port': '14002'
+    'command_port': 11002,
+    'drone_port': 8889,
+    'data_port': 13002,
+    'video_port': 14002
 }]
 
 vloc_args = [{
@@ -37,7 +37,7 @@ vloc_args = [{
     'map_init_pose_z': -0.035,
     # 'camera_frame_id': 'camera_link' + suffix,
     # 'base_odometry_pub_topic': 'filtered_odom',
-    # 'sub_camera_info_best_effort_not_reliable': 1,
+    'sub_camera_info_best_effort_not_reliable': 1,
     'publish_tfs_per_marker': 0,
     'publish_image_marked': 1,
     'sam_not_cv': sam_not_cv,
@@ -59,7 +59,7 @@ def generate_launch_description():
     entities = [
         ExecuteProcess(cmd=['rviz2', '-d', rviz_config_filename], output='screen'),
 
-        Node(package='tello_driver', node_executable='tello_driver', output='screen',
+        Node(package='tello_driver', node_executable='tello_driver_main', output='screen',
              parameters=tello_args),
 
         Node(package='fiducial_vlam', node_executable='vloc_node', output='screen',
